da284e80bd2e0b4cae170acea5292a173ad7ded7
Lalith <lalithreddy2004@gmail.com>
2026-08-11 20:51:33 +0530

    Implemented frontend and configured CORS in backend (*) ! Everything is stable

--- src/main/java/com/email/emailwritersb/Controller/EmailGeneratorController.java
+++ src/main/java/com/email/emailwritersb/Controller/EmailGeneratorController.java
@@ -7,14 +7,12 @@ import com.email.emailwritersb.Service.EmailGeneratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins="*")
 public class EmailGeneratorController {
 
 
